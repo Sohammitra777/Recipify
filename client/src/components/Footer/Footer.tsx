7604d662ae8dc35001308cadaa051741f36d0cb9
sohammitra777 <soham.business777@gamil.com>
2025-11-18 21:50:19 +0530

    general improvements

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ function Footer(){
     return (
         <footer>
             <a href="https://github.com/Sohammitra777" target='blank'>GitHub</a>
-            <a href="https://www.linkedin.com/in/soham-mitra-a73b782b3/" target='blank'>LinkedIN</a>
+            <a href="https://www.linkedin.com/in/soham-mitra-a73b782b3/" target='blank'>Linked-In</a>
             <a href="mailto:soham.business777@gamil.com" target='blank'>Email</a>
         </footer>
     );
