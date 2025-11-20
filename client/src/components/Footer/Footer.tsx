4f8410ad14c6ea5231247e787e1ca9fec34c2bef
sohammitra777 <soham.business777@gamil.com>
2025-11-20 17:08:43 +0530

    code mentioned

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.css'
 function Footer(){
     return (
         <footer>
-            <a href="https://github.com/Sohammitra777" target='blank'>GitHub</a>
+            <a href="https://github.com/Sohammitra777/Recipify" target='blank'>GitHub & Code</a>
             <a href="https://www.linkedin.com/in/soham-mitra-a73b782b3/" target='blank'>Linked-In</a>
             <a href="mailto:soham.business777@gamil.com" target='blank'>Email</a>
         </footer>
