5df181eca445ac4db0238d10d7fc1fe2b88275f7
sohammitra777 <soham.business777@gamil.com>
2025-11-20 20:19:49 +0530

    api url update and configured for render

--- client/src/app/features/ingredients/ingredientThunks.ts
+++ client/src/app/features/ingredients/ingredientThunks.ts
@@ -5,7 +5,7 @@ const fetchRecipe = createAsyncThunk<string, string[], { rejectValue: string }>(
     "recipe/fetchRecipe",
     async (ingredient , { rejectWithValue }) => {
         try {
-            const response = await axios.post("http://localhost:3000/api/data", 
+            const response = await axios.post("https://recipify-express.onrender.com/api/data", 
                 {ingredient}
             );
 
